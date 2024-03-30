70f5e08a01e69e8b06f9584e254184d6d7339bd1
Rowsen <bayramovw0@gmail.com>
2024-03-30 20:14:17 +0500

    forms test. created log in forms

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html"), 500123
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if username and password:
+            if username == "admin" and password == "adminadmin":
+                return "You logged in!"
+            return "You sent wrong account credentials!"
+
+    return render_template('auth/login.html')
 
 
 if __name__ == "__main__":
